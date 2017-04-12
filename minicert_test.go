170978f419bfd63b520e9cde3631673ce60efd3c
Richard Barnes <rlb@ipv.sx>
2017-04-11 20:06:55 -0400

    Test skeleton

---
+++ minicert_test.go
@@ -0,0 +1,29 @@
+package minicert
+
+import (
+	"testing"
+)
+
+func TestAttributeMarshalUnmarshal(t *testing.T) {
+	// TODO
+}
+
+func TestEEMarshalUnmarshal(t *testing.T) {
+	// TODO
+}
+
+func TestCAMarshalUnmarshal(t *testing.T) {
+	// TODO
+}
+
+func TestFindPaths(t *testing.T) {
+	// TODO
+}
+
+func TestVerifyChain(t *testing.T) {
+	// TODO
+}
+
+func TestVerify(t *testing.T) {
+	// TODO
+}
